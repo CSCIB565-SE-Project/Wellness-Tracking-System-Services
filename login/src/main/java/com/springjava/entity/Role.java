ae1ea292d787e24228ac8fe64b85ea11a93c126b
Prinston Rebello <prinstonrebello@gmail.com>
2024-02-18 15:06:26 -0500

    Adding entity class for login service

---
+++ login/src/main/java/com/springjava/entity/Role.java
@@ -0,0 +1,36 @@
+package com.springjava.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String roleName;
+
+    public Role(){
+
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getRoleName(){
+        return roleName;
+    }
+
+    public void setRole(String roleName){
+        this.roleName = roleName;
+    }
+}
---
+++ login/src/main/java/com/springjava/entity/User.java
@@ -0,0 +1,113 @@
+package com.springjava.entity;
+
+import java.sql.Date;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String fname;
+    @Column(nullable = true)
+    private String mname;
+    @Column(nullable = false)
+    private String lname;
+    @Column(nullable = false)
+    private Date dob;
+    @Column(nullable = false)
+    private String gender;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Role role;
+    private EntityManager entityManager;
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setFname(String fname){
+        this.fname = fname;
+    }
+
+    public String getFname(){
+        return fname;
+    }
+
+    public void setMname(String mname){
+        this.mname = mname;
+    }
+
+    public String getMname(){
+        return mname;
+    }
+
+    public void setLname(String lname){
+        this.lname = lname;
+    }
+
+    public String getLname(){
+        return lname;
+    }
+    
+    public void setDoB(Date dob){
+        this.dob = dob;
+    }
+
+    public Date getDoB(){
+        return dob;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getFnameById(Integer id){
+        User user = entityManager.find(User.class, id);
+        if(user != null){
+            return user.getFname();
+        }
+        else{
+           return null; 
+        }
+    }    
+}
