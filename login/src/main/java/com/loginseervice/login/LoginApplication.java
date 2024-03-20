0a843ba2e96e5b660f4dbd00c9777501161b53b3
pradyu99 <pradyu99.reddy@gmail.com>
2024-03-20 00:22:10 -0400

    SessionManagement

--- login/src/main/java/com/loginseervice/login/LoginApplication.java
+++ login/src/main/java/com/loginseervice/login/LoginApplication.java
@@ -2,6 +2,8 @@ package com.loginseervice.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @SpringBootApplication
 public class LoginApplication {
@@ -9,5 +11,10 @@ public class LoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
+
+	 @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
 
 }
--- login/src/main/java/com/loginseervice/login/security/UserRegistrationSecurityConfig.java
+++ login/src/main/java/com/loginseervice/login/security/UserRegistrationSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,19 +16,34 @@ import org.springframework.security.web.SecurityFilterChain;
 public class UserRegistrationSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(requests -> requests.requestMatchers("/register/**").permitAll())
-                .authorizeHttpRequests(requests -> requests.requestMatchers("/login/**").permitAll())
-                .authorizeHttpRequests(requests -> requests.requestMatchers("/password-reset/**").permitAll())
-                .authorizeHttpRequests(requests -> requests.requestMatchers("/users/**")
-                
-                .hasAnyAuthority("USER", "ADMIN")).build();
-                //formLogin(Customizer.withDefaults()).build();
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+        .cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
+        .authorizeRequests(authorize -> authorize
+        .requestMatchers("/register/**").permitAll()
+        .requestMatchers("/login/**").permitAll()
+        .requestMatchers("/password-reset/**").permitAll()
+        .requestMatchers("/users/**").hasAnyAuthority("USER", "ADMIN")
+        )
+        .sessionManagement(session -> session
+            .sessionFixation().migrateSession() // Ensure session fixation protection
+            .invalidSessionUrl("/login") // Redirect to login page for invalid sessions
+            .maximumSessions(1).expiredUrl("/login?expired") // Allow only one session per user
+        )
+        .formLogin(Customizer.withDefaults()) // Enable form login
+        .build();
+
     }
 }
