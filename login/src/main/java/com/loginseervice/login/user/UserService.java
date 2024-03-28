005aaec2701ee45c4b3d3705288890e7113b2928
pradyu99 <pradyu99.reddy@gmail.com>
2024-03-28 01:08:26 -0400

    OAuth & JWT tokens

--- login/src/main/java/com/loginseervice/login/user/UserService.java
+++ login/src/main/java/com/loginseervice/login/user/UserService.java
@@ -20,14 +20,16 @@ import com.loginseervice.login.registration.token.PasswordResetToken;
 import com.loginseervice.login.registration.token.PasswordResetTokenRepository;
 import com.loginseervice.login.registration.token.VerificationToken;
 import com.loginseervice.login.registration.token.VerificationTokenRepository;
+import com.loginseervice.login.security.JwtService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements IUserService{
+public class UserService implements IUserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     private final VerificationTokenRepository tokenRepository;
     private final PasswordResetTokenRepository passwordResetTokenRepository;
 
@@ -39,7 +41,7 @@ public class UserService implements IUserService{
     @Override
     public User registUser(RegistrationRequest request) {
         Optional<User> user = this.findByEmail(request.email());
-        if(user.isPresent()){
+        if (user.isPresent()) {
             throw new UserAlreadyExistsException("User with Email " + request.email() + " already exists!!");
         }
         var newUser = new User();
@@ -54,14 +56,14 @@ public class UserService implements IUserService{
         newUser.setRole(request.role());
         return userRepository.save(newUser);
     }
-    
+
     @Override
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
     @Override
-    public String getPasswordEmail(Optional<User> user){
+    public String getPasswordEmail(Optional<User> user) {
         User usr = user.get();
         String password = usr.getPassword();
         return password;
@@ -76,12 +78,12 @@ public class UserService implements IUserService{
     @Override
     public String validateToken(String token) {
         VerificationToken theToken = tokenRepository.findByToken(token);
-        if(theToken == null){
+        if (theToken == null) {
             return "Invalid Verification Token";
         }
         User user = theToken.getUser();
         Calendar calendar = Calendar.getInstance();
-        if(theToken.getExpirationTime().getTime() - calendar.getTime().getTime() <= 0) {
+        if (theToken.getExpirationTime().getTime() - calendar.getTime().getTime() <= 0) {
             tokenRepository.delete(theToken);
             return "Token Already Expired!";
         }
@@ -96,35 +98,34 @@ public class UserService implements IUserService{
         User usr = null;
         Boolean status = false;
         Optional<User> user = this.findByEmail(request.email());
-        if(user != null){
-            if(user.get().isEnabled()){
+        if (user.isPresent()) {
+            if (user.get().isEnabled()) {
                 String password = request.password();
                 String encodedPassword = this.getPasswordEmail(user);
                 Boolean isPwdMatch = passwordEncoder.matches(password, encodedPassword);
-                if(isPwdMatch){
-                    Optional<User> tempUser = userRepository.findOneByEmailAndPassword(request.email(), encodedPassword);
-                    if(tempUser.isPresent()){
-                        statusMessage = "Login Success";
-                        usr = tempUser.get();
-                        status = true;
-                    }
-                    else{
-                        statusMessage = "Login Failed";
-                    }
-                }
-                else{
+                if (isPwdMatch) {
+                    usr = user.get();
+                    status = true;
+                    statusMessage = "Login Success";
+                } else {
                     statusMessage = "Password Mismatch";
                 }
-            }
-            else{
+            } else {
                 statusMessage = "User yet to be verified!";
             }
-        }
-        else{
+        } else {
             statusMessage = "User does not exist";
         }
-        return new LoginResponse(statusMessage, status, usr);
-    }    
+        if (status) {
+            String token = jwtService.GenerateToken(usr.getUsername());
+            return new LoginResponse(statusMessage, status, usr, token);
+        } else {
+            // Return LoginResponse with null user and token
+            return new LoginResponse(statusMessage, status, null, null);
+        }
+    }
+    
+
     @Override
     public void createPasswordResetTokenForUser(String email, String appUrl) {
         User user = userRepository.findByEmail(email)
@@ -134,26 +135,24 @@ public class UserService implements IUserService{
         PasswordResetToken existingToken = passwordResetTokenRepository.findByUser(user);
         if (existingToken != null) {
             existingToken.setToken(token);
-            existingToken.setExpiryDate(LocalDateTime.now().plusHours(1)); // Reset the expiry date
+            existingToken.setExpiryDate(LocalDateTime.now().plusHours(1));
             passwordResetTokenRepository.save(existingToken);
         } else {
             PasswordResetToken resetToken = new PasswordResetToken();
             resetToken.setToken(token);
             resetToken.setUser(user);
-            resetToken.setExpiryDate(LocalDateTime.now().plusHours(1)); // Token expires in 1 hour
+            resetToken.setExpiryDate(LocalDateTime.now().plusHours(1));
             passwordResetTokenRepository.save(resetToken);
         }
 
         sendPasswordResetEmail(user, appUrl, token);
-        }
+    }
 
-
-    
     @Autowired
     private JavaMailSender mailSender;
+
     @Override
     public void sendPasswordResetEmail(User user, String appUrl, String token) {
-        // This is the link where the email will direct the user to
         String url = "http://localhost:3000/reset-password?token=" + token;
         String subject = "Password Reset Request";
         String content = "To reset your password, click the link below:\n" + url;
@@ -165,17 +164,16 @@ public class UserService implements IUserService{
         mailSender.send(email);
     }
 
-    
+    @Override
     public boolean resetPassword(String token, String newPassword) {
         PasswordResetToken resetToken = passwordResetTokenRepository.findByToken(token);
         if (resetToken == null || resetToken.getExpiryDate().isBefore(LocalDateTime.now())) {
-            return false; // Token is invalid or expired
+            return false;
         }
         User user = resetToken.getUser();
-        user.setPassword(passwordEncoder.encode(newPassword)); // Encode and update the new password
+        user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
-        passwordResetTokenRepository.delete(resetToken); // Invalidate the token
+        passwordResetTokenRepository.delete(resetToken);
         return true;
     }
-
 }
