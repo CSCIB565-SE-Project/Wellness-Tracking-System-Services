88c6323d831c753d859b5bdfeb7eed560a5bd88d
Prinston Rebello <prinstonrebello@gmail.com>
2024-04-20 17:30:56 -0400

    added cors policy

--- oauthlogin/src/app.js
+++ oauthlogin/src/app.js
@@ -3,6 +3,16 @@ var authRouter = require('./routes/auth');
 var requestRouter = require('./routes/request');
 var app = express();
 
+app.options('*', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Headers", ['X-Requested-With',
+  'content-type','credentials']);
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.status(200);
+  next()
+})
+
 app.use('/auth', authRouter);
 app.use('/request', requestRouter);
 
