468b930cc7339e4c5140fe451a2c7930beab5202
Prinston Rebello <prinstonrebello@gmail.com>
2024-04-21 00:27:00 -0400

    fixed oauth

--- oauthlogin/src/routes/auth.js
+++ oauthlogin/src/routes/auth.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const dotenv = require('dotenv');
-const passport = require('passport');
 dotenv.config();
 const {OAuth2Client} = require('google-auth-library');
 
