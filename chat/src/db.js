35ce2e5c6a715b1d3229d2335bbfddc1a1d1ce26
Prinston Rebello <prinstonrebello@gmail.com>
2024-04-21 16:23:09 -0400

    chat fix on server

--- chat/src/db.js
+++ chat/src/db.js
@@ -9,7 +9,10 @@ const pool = mysql.createPool({
   user            : process.env.DB_USER, 
   password        : process.env.DB_PASS, 
   database        : process.env.DB_NAME, 
-  port            : 3306
+  port            : 3306,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'DigiCertGlobalRootCA.crt.pem'))
+  }
 });
 
 
