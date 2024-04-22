7f66adb8c660a02d9e8c3b2256be028a01b13cef
Krina Shah <krinas393@gmail.com>
2024-04-21 21:34:51 -0400

    converted objectid to string for id

--- dashboard/src/main/java/com/dashboard/user/TimetableRepository.java
+++ dashboard/src/main/java/com/dashboard/user/TimetableRepository.java
@@ -3,10 +3,9 @@ package com.dashboard.user;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TimetableRepository extends MongoRepository<Timetable, ObjectId> {
+public interface TimetableRepository extends MongoRepository<Timetable, String> {
     Optional<Timetable> findById(String id);
     List<Timetable> findByUserId(Integer userId);
 }
