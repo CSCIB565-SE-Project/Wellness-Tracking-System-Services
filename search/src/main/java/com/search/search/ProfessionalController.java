e92df39467ef6b335cdd555f56ebb1975b11abc4
pradyu99 <pradyu99.reddy@gmail.com>
2024-04-23 04:42:37 -0400

    search

--- search/src/main/java/com/search/search/ProfessionalController.java
+++ search/src/main/java/com/search/search/ProfessionalController.java
@@ -3,6 +3,7 @@ package com.search.search;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,19 +14,14 @@ import com.search.user.UserService;
 
 @RestController
 @RequestMapping("/professionals")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProfessionalController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/search")
-    public List<Trainer> searchProfessionals(
-            @RequestParam(required = false) String userId,
-            @RequestParam(required = false) String firstName,
-            @RequestParam(required = false) String username,
-            @RequestParam(required = false) String specialty,
-            @RequestParam(required = false) String gender,
-            @RequestParam(required = false) String location) {
-        return userService.searchProfessionals(userId, firstName, username, specialty, gender, location);
+    public List<Trainer> searchProfessionals(@RequestParam String specialty) { 
+        return userService.searchProfessionals(specialty);
     }
 }
--- search/src/main/java/com/search/user/TrainerRepository.java
+++ search/src/main/java/com/search/user/TrainerRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TrainerRepository extends MongoRepository<Trainer, String> {
-    List<Trainer> findByUserIdContainingIgnoreCaseOrFirstNameContainingIgnoreCaseOrUsernameContainingIgnoreCaseOrSpecialtyContainingIgnoreCaseOrGenderContainingIgnoreCaseOrLocationContainingIgnoreCase(String userId, String firstName, String username, String specialty, String gender, String location);
+    List<Trainer> findBySpecialtyContainingIgnoreCase(String specialty);
 }
