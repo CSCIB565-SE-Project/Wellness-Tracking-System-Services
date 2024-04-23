0d4fd35ec4b16deb757b34622617edebf36d1282
pradyu99 <pradyu99.reddy@gmail.com>
2024-04-23 13:41:32 -0400

    class files

--- login/src/main/java/com/loginseervice/login/security/JwtService.java
+++ login/src/main/java/com/loginseervice/login/security/JwtService.java
@@ -64,7 +64,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userid)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
